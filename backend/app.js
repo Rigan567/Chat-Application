ca321a326c9b097a0c72a38b1f4c47811e095937
unknown <rigan.paul@g.bracu.ac.bd>
2024-10-08 03:38:04 +0600

    Initial commit with frontend and backend

---
+++ backend/app.js
@@ -0,0 +1,89 @@
+//External Imports
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const moment = require("moment");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const loginRouter = require("./router/loginRouter");
+const usersRouter = require("./router/usersRouter");
+const inboxRouter = require("./router/inboxRouter");
+const cors = require("cors");
+
+//internal imports
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandlers");
+// const { checkLogin } = require("./middlewares/common/checkLogin");
+
+dotenv.config();
+
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // Allow requests from your frontend
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("disconnect", () => {});
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+app.locals.moment = moment;
+
+//db connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(
+      `DB connected successfully on ${process.env.MONGO_CONNECTION_STRING} `
+    );
+  })
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//set static folder
+app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
+
+//parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// app.use(checkLogin);
+
+//routing setup
+app.use("/", loginRouter);
+app.use("/users", usersRouter);
+app.use("/inbox", inboxRouter);
+
+//404 error handling
+app.use(notFoundHandler);
+
+//common error handling
+app.use(errorHandler);
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT} `);
+});
